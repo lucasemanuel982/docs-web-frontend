3c22d789601701913e4f8f1e1f80d4ce760e449d
lucasEmanuel982 <lucasemanuel982@gmail.com>
2025-08-26 19:42:59 -0300

    Adição da porta no env para o render

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: import.meta.env.VITE_PORT || 3000,
     proxy: {
       '/api': {
         target: 'https://docs-web-backend.onrender.com',
